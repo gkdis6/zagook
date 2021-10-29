6f0530a563ea226876e622e41dcf447a5207fa65
SangYong <gkdis6@gmail.com>
2021-10-29 10:58:32 +0900

    create workspace

---
+++ workspace/zagook/src/main/java/com/project/zagook/ZagookApplication.java
@@ -0,0 +1,13 @@
+package com.project.zagook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZagookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZagookApplication.class, args);
+	}
+
+}
---
+++ workspace/zagook/src/test/java/com/project/zagook/ZagookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.zagook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZagookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
