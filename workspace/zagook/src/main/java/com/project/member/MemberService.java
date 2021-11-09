9c15bd65b2bc8a80ea99a18381a9e3b0dd444ee9
yh77yh <yh77yh@gmail.com>
2021-11-09 17:13:15 +0900

    testttt

--- workspace/zagook/src/main/java/com/project/member/MemberService.java
+++ workspace/zagook/src/main/java/com/project/member/MemberService.java
@@ -1,9 +1,6 @@
 package com.project.member;
 
-<<<<<<< HEAD
-public class MemberService {
-=======
-import java.util.List;
+
 import java.util.Map;
 
 public interface MemberService {
@@ -17,6 +14,6 @@ public interface MemberService {
 	int duplicatedEmail(String email);
 
 	int create(MemberDTO dto);
->>>>>>> e638b8a2bf4c08cbb46d6772d85a407325657a51
+
 
 }
--- workspace/zagook/src/main/java/com/project/member/MemberServiceImpl.java
+++ workspace/zagook/src/main/java/com/project/member/MemberServiceImpl.java
@@ -1,6 +1,5 @@
 package com.project.member;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
