819c1733cfc661a6c5892bd2ce10f3e959a63e9b
JeilJeong <42158390+JeilJeong@users.noreply.github.com>
2021-11-25 11:24:38 +0900

    add cursor:pointer to searchbar autocomplete li

--- workspace/zagook/src/main/resources/static/js/feed/onclick_processing.js
+++ workspace/zagook/src/main/resources/static/js/feed/onclick_processing.js
@@ -3,7 +3,7 @@ function onclickTag(event) {
 		openSearchbar();
 	let tag_name = event.target.innerText;
 	console.log(tag_name);
-	$("#searchInput").val(tag_name.substr(1, tag_name.length))
+	$("#searchInput").val(tag_name.substr(1, tag_name.length));
 	$("#searchInput").focus();
 }
 
