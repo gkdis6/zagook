2123eb2e19620bb64fd07e0dad2d64d46908e9a8
SangYong <gkdis6@gmail.com>
2021-11-26 17:57:43 +0900

    add friend container

--- workspace/zagook/src/main/java/com/project/friend/FriendController.java
+++ workspace/zagook/src/main/java/com/project/friend/FriendController.java
@@ -1,58 +1,36 @@
 package com.project.friend;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.project.reply.ReplyDTO;
-import com.project.reply.ReplyMapper;
 
 @Controller
 public class FriendController {
-	
 	@Autowired
-	private ReplyMapper mapper;
+	@Qualifier("com.project.friend.FriendServiceImpl")
+	private FriendService service;
 	
 	@GetMapping("/friend")
-	public String newFriendForm() {
-		return "/friend";
-	}
-	@GetMapping("/reply/read")
-	public String read(int contentsno, Model model, HttpServletRequest request) {
+	public String friend(HttpServletRequest request, HttpSession session) {
+		if (session.getAttribute("id") != null) {
+			Map map = new HashMap();
+			String id = (String) session.getAttribute("id");
 
-		mapper.upViewcnt(contentsno);
-		ReplyDTO dto = mapper.read(contentsno);
+			map.put("id", id);
 
-		String content = dto.getContent().replaceAll("\r\n", "<br>");
-		dto.setContent(content);
-
-		model.addAttribute("dto", dto);
+			List<FriendDTO> list = service.friendList(map);
+			
+			request.setAttribute("list", list);
+		}
 		
-        /* 댓글 관련 시작 */
-        int nPage = 1;
-        if (request.getParameter("nPage") != null) {
-                nPage = Integer.parseInt(request.getParameter("nPage"));
-        }
-        int recordPerPage = 3;
-
-        int sno = ((nPage - 1) * recordPerPage) + 1;
-        int eno = nPage * recordPerPage;
-
-        Map map = new HashMap();
-        map.put("sno", sno);
-        map.put("eno", eno);
-        map.put("nPage", nPage);
-
-        model.addAllAttributes(map);
-
-        /* 댓글 처리 끝 */
-
-		return "/reply/read";
+		return "/friend";
 	}
 }
--- workspace/zagook/src/main/java/com/project/friend/FriendService.java
+++ workspace/zagook/src/main/java/com/project/friend/FriendService.java
@@ -3,7 +3,7 @@ package com.project.friend;
 import java.util.List;
 import java.util.Map;
 
-public interface FriendMapper {
+public interface FriendService {
 
 	List<FriendDTO> friendList(Map map);
 
