268a068dfafd78dde9904bc1ba0a740230f67090
SangYong <gkdis6@gmail.com>
2021-11-29 16:37:02 +0900

    fix conflict

--- src/main/java/com/project/feed/FeedServiceImpl.java
+++ src/main/java/com/project/feed/FeedServiceImpl.java
@@ -170,6 +170,7 @@ public class FeedServiceImpl implements FeedService {
 	public int submit_friend2(Map map) {
 		// TODO Auto-generated method stub
 		return mapper.submit_friend2(map);
+	}
 	@Override
 	public int delete_feed(Map map) {
 		// TODO Auto-generated method stub
