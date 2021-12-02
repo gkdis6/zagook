43654692ffd0c0e5b8790bc7c833af04e778a1f1
jaehong5915 <woghdclf@naver.com>
2021-12-02 17:33:38 +0900

    edit-create.js

--- src/main/resources/static/js/member/create.js
+++ src/main/resources/static/js/member/create.js
@@ -1,3 +1,18 @@
+$('#tel').keydown(function(event) {
+    var key = event.charCode || event.keyCode || 0;
+    $text = $(this);
+    if (key !== 8 && key !== 9) {
+        if ($text.val().length === 3) {
+            $text.val($text.val() + '-');
+        }
+        if ($text.val().length === 8) {
+            $text.val($text.val() + '-');
+        }
+    }
+ 
+    return (key == 8 || key == 9 || key == 46 || (key >= 48 && key <= 57) || (key >= 96 && key <= 105));          
+});
+
 const items = document.querySelectorAll('.question');
 
   function openCloseAnswer() {
