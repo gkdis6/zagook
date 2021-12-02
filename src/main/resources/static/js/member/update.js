5d432f8e5c46f066cdaf48c15ba0c67769ae9aae
jaehong5915 <woghdclf@naver.com>
2021-12-02 14:57:35 +0900

    edit-updateform

--- src/main/resources/static/js/member/update.js
+++ src/main/resources/static/js/member/update.js
@@ -14,3 +14,20 @@ $('#tel').keydown(function(event) {
 });
 
 
+const items = document.querySelectorAll('.question');
+
+  function openCloseAnswer() {
+    const answerId = this.id.replace('que', 'ans');
+
+    if(document.getElementById(answerId).style.display === 'block') {
+      document.getElementById(answerId).style.display = 'none';
+      document.getElementById(this.id + '-toggle').textContent = '∧';
+//      document.getElementById(this.id + '-toggle').textContent = '+';
+    } else {
+      document.getElementById(answerId).style.display = 'block';
+      document.getElementById(this.id + '-toggle').textContent = '∨';
+//      document.getElementById(this.id + '-toggle').textContent = '-';
+    }
+  }
+
+  items.forEach(item => item.addEventListener('click', openCloseAnswer));
